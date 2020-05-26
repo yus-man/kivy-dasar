c8a565a13b9d580ca405a77cc577035724ac9352
yus-man <65966465+yus-man@users.noreply.github.com>
2020-05-26 22:37:01 +0700

    Dasar Kivy
    Membuat label

---
+++ label.py
@@ -0,0 +1,31 @@
+"""
+Kivy dasar:
+Menampilkan label di GUI
+
+Langkah-langkah:
+"""
+
+# Mengimport App dari kivy.app
+from kivy.app import App
+
+# Import Label untuk menampilkan judul dari kivy.uix.label
+from kivy.uix.label import Label
+
+
+# Buat class aplikasi yang mewarisi dari App yang ada di kivy
+
+class AplikasiKu(App):
+	# Buatlah method builder nya
+	def build(self):
+		"""
+		Agar label bisa ditampilkan ke GUI
+		maka return nilai labelnya dengan sebuah text
+		"""
+		return Label(text = 'Selamat datang')  # taks ini yang nanti akan tampil di GUI
+
+# Jalankan aplikasi yang telah dibuat
+AplikasiKu().run()
+
+
+
+
